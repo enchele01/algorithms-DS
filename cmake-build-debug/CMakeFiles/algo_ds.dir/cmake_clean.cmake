3e012a3d6a321a79d0785bb07bfa6d9185feeb37
depth01 <doninura95@gmail.com>
2022-11-09 14:31:20 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/algo_ds.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algo_ds.dir/main.cpp.obj"
+  "CMakeFiles/algo_ds.dir/recursion/code_01.cpp.obj"
+  "algo_ds.exe"
+  "algo_ds.exe.manifest"
+  "algo_ds.pdb"
+  "libalgo_ds.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algo_ds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ recursion/code_01.cpp
@@ -0,0 +1,14 @@
+// code_01
+#include "iostream"
+using namespace std;
+void fun1(int n){
+    if(n>0){
+        printf("%d",n);
+        fun1(n-1);
+    }
+}
+int main()
+{
+    fun1(3);
+    return 0;
+}
